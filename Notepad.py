012406baa18717a3796daae4949c91ad5cf8be27
George Chan <ygchan89@gmail.com>
2020-03-28 23:48:46 -0400

    Update

--- Notepad.py
+++ Notepad.py
@@ -6,3 +6,6 @@
 
 # Additional Data
 # https://www1.nyc.gov/site/doh/covid/covid-19-main.page
+
+# https://www1.nyc.gov/site/doh/health/health-topics/coronavirus.page
+# Case Count as of 03/28/2020 = 30765
